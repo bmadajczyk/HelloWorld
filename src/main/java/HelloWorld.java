61c8c231ba18ecf4849c59e5c7ffeb002d028cbb
bmadajczyk <madajczykb@gmail.com>
2017-08-22 13:21:28 +0200

    Hello World commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by bartek on 2017-08-22.
+ */
+@Controller
+@EnableAutoConfiguration
+public class HelloWorld {
+    @RequestMapping("/")
+    @ResponseBody
+    String home(){
+        return "Hello World";
+    }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorld.class , args);
+
+    }
+
+}
